43a6aa6b9f4ce9ab74f23ff102d6013e88b29721
phlco <phillip.lamplugh@gmail.com>
2014-01-13 15:38:23 -0500

    working on postgres

--- scripts/postgres.sh
+++ scripts/postgres.sh
@@ -5,5 +5,7 @@ brew install postgres
 initdb /usr/local/var/postgres
 
 # set up launch agent?
-
+# cp /usr/local/Cellar/postgresql/$pg_latest/homebrew.mxcl.postgresql.plist ~/Library/LaunchAgents/
+# launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
+# pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start
 # createdb?
