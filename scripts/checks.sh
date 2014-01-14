bf55007ea03ec7dfba675e64fd17b2d58b78afce
phlco <phillip.lamplugh@gmail.com>
2014-01-14 11:18:32 -0500

    adds tests for checks

---
+++ scripts/checks.sh
@@ -0,0 +1,20 @@
+##Checkpoints to see if you computer is configured properly.
+
+# When you type `brew doctor` into your Terminal, you get no warnings.  If you get warnings follow the instructions and repeat until it says `Your system is ready to brew.`
+
+# When you type `ruby -v` into your terminal, you get `ruby  2.1.0`
+
+# When you type `pry` into your terminal, it changes the prompt to `pry(main)`
+# (Type `exit` to return to return to the bash prompt)
+gem list pry -i # returns true
+# When you type `subl` into your terminal, it opens up Sublime Text
+
+# You have your `.gitignore_global`, `.gitconfig`, `.bash_profile` set up in your root directory. You should be able to open each one with `subl ~/FILENAME` ie `subl ~/.gitignore_global`
+
+# When you have Sublime open and press `CMD+Shift+P` you'll see a drop down menu. When you type `install` into the menu, you should see the option `Package Control: Install Package`
+
+# Your ssh keys are set up (see [here](https://help.github.com/articles/generating-ssh-keys) for instructions)
+
+# When you type `subl ~/dev/wdi/WDI_NYC_12`, you see our class repository files
+
+# You have `Spectacle` installed on your computer and can use it to manage your windows
--- scripts/git.sh
+++ scripts/git.sh
@@ -17,8 +17,8 @@ read github_email
 # writing name and email to gitconfig
 # http://stackoverflow.com/questions/17957196/using-variables-in-bash-script-to-set-ini-file-values-while-executing
 # sed "/^number=/s/=.*/=5/" baseinput.ini > input.ini
-sed -e s/github_name/"$github_name"/ -e s/github_email/"$github_email"/ ../dotfiles/gitconfig > ~/.gitconfig
+sed -e s/github_name/"$github_name"/ -e s/github_email/"$github_email"/ ../settings/git/gitconfig > ~/.gitconfig
 
 # copying gitignore global and commit template
-cp ../dotfiles/gitignore_global ~/.gitignore_global
-cp ../dotfiles/gitmessage.txt ~/.gitmessage.txt
+cp ../settings/git/gitignore_global ~/.gitignore_global
+cp ../settings/git/gitmessage.txt ~/.gitmessage.txt
