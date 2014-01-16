163a08a401c76548a9ddc56b0c5f3306da4a9d12
phlco <phillip.lamplugh@gmail.com>
2014-01-16 12:48:25 -0500

    switches install order

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
 echo "Welcome to Installfest"
+sudo echo "Thanks." # PJ: capture the user's password
 echo "Enter your full name:"
 read user_name
 echo "Stop here for a second."
-sudo echo "Thanks." # PJ: capture the user's password
 
 # echo "Enter your local password:"
 # read -s user_pass
