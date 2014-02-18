c5591789bbcee6c6eaf84708a235637e345b6dce
phlco <phillip.lamplugh@gmail.com>
2014-02-18 15:04:56 -0500

    simplifies terminal solarized

--- scripts/terminal.sh
+++ scripts/terminal.sh
@@ -1,19 +1,10 @@
 ####################################################################################
 # Download and set Terminal colors to Solarized
 ####################################################################################
-declare term_default=$(defaults read com.apple.terminal "Default Window Settings")
-if [[ $term_default != *Solarized* ]]; then
-  echo "Installing 'Solarized Colors' for Terminal..."
-  curl -L -o "solarized.zip" https://github.com/tomislav/osx-terminal.app-colors-solarized/archive/master.zip;
-  unzip "solarized.zip"
-  open "osx-terminal.app-colors-solarized-master/Solarized Light.terminal"
-  sleep 1
-  open "osx-terminal.app-colors-solarized-master/Solarized Dark.terminal"
-  sleep 1
-  defaults write com.apple.terminal "Default Window Settings" -string "Solarized Dark"
-  defaults write com.apple.terminal "Startup Window Settings" -string "Solarized Dark"
-  rm solarized.zip
-  rm -rf "osx-terminal.app-colors-solarized-master"
-else
-  echo "Solarized Colors already installed";
-fi
+echo "Settings Solarized Colors for Terminal..."
+open "$SETTINGS/dotfiles/Solarized Light.terminal"
+sleep 1
+open "$SETTINGS/dotfiles/Solarized Dark.terminal"
+sleep 1
+defaults write com.apple.terminal "Default Window Settings" -string "Solarized Dark"
+defaults write com.apple.terminal "Startup Window Settings" -string "Solarized Dark"
